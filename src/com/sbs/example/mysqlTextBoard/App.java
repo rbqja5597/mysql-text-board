0b77d8f02b294a2985117a96390d46bde0812031
jhs512 <jangka512@gmail.com>
2020-11-12 11:43:54 +0900

    기본적인 명령어 처리 기반 마련

---
+++ src/com/sbs/example/mysqlTextBoard/App.java
@@ -0,0 +1,21 @@
+package com.sbs.example.mysqlTextBoard;
+
+import java.util.Scanner;
+
+public class App {
+	public void run() {
+		Scanner sc = Container.scanner;
+
+		while (true) {
+			System.out.printf("명령어) ");
+			String cmd = sc.nextLine();
+
+			if (cmd.equals("article list")) {
+				System.out.println("== 게시물 리스트 ==");
+			} else if (cmd.equals("system exit")) {
+				System.out.println("== 시스템 종료 ==");
+				break;
+			}
+		}
+	}
+}
--- src/com/sbs/example/mysqlTextBoard/Main.java
+++ src/com/sbs/example/mysqlTextBoard/Main.java
@@ -2,6 +2,6 @@ package com.sbs.example.mysqlTextBoard;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("안녕");
+		new App().run();
 	}
 }
